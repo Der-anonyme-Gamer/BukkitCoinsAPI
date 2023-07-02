80a26c75cd3d09844bab4d99147e89df3884a546
Der-anonyme-Gamer <132397555+Der-anonyme-Gamer@users.noreply.github.com>
2023-07-02 18:50:52 +0200

    Update CoinsAPI.java

--- CoinsAPI.java
+++ CoinsAPI.java
@@ -10,45 +10,38 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class CoinsAPI implements CommandExecutor {
+public  class CoinsAPI {
+
     private Player p;
     private Long Coins;
-    public CoinsAPI(Player p,Long coins){
-    this.Coins=coins;
-    this.p=p;
+
+    public CoinsAPI(Player p, Long coins) {
+        this.Coins = coins;
+        this.p = p;
     }
-    
-public Long getCoins(){
+
+    public Long getCoins() {
         return Coins;
-}
-public void addCoins(Long coins){
-        Coins+=coins;
-}
-// Return false if player can´t do operation
-    public boolean removeCoins(Long coins){
-        if(Coins>=coins){
-            Coins-=coins;
+    }
+
+    public void addCoins(Long coins) {
+        Coins += coins;
+    }
+
+    // Return false if player can´t do operation
+    public boolean removeCoins(Long coins) {
+        if (Coins >= coins) {
+            Coins -= coins;
             return true;
-        }else{
-            Coins=0L;
+        } else {
+            Coins = 0L;
             return false;
         }
     }
-    public void setCoins(Long coins){
-        Coins=coins;
+
+    public void setCoins(Long coins) {
+        Coins = coins;
     }
-    @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-      if(!(commandSender instanceof Player))return false;
-       if(command.getName().equalsIgnoreCase("money")){
-showMoney();
-       }else if(command.getName().equalsIgnoreCase("pay")){
-          pay();
-       }
-        return false;
-    }
-// Method to show player´s coins
-    protected abstract void showMoney();
-    // Method to make a pay action
-    public abstract void pay();
+
+
 }
