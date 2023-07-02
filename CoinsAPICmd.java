3e3408933e710e52e3f5f4489b4f420406ffca93
Der-anonyme-Gamer <132397555+Der-anonyme-Gamer@users.noreply.github.com>
2023-07-02 18:49:48 +0200

    Create CoinsAPICmd.java

---
+++ CoinsAPICmd.java
@@ -0,0 +1,27 @@
+package [DeinPackage];
+/*
+Copyright:
+2023
+Felix H
+  */
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class CoinsAPICmd implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(!(commandSender instanceof Player))return false;
+        if(command.getName().equalsIgnoreCase("money")){
+            showMoney();
+        }else if(command.getName().equalsIgnoreCase("pay")){
+            pay();
+        }
+        return true;
+    }
+protected abstract void showMoney();
+    protected abstract void pay();  
+    
+}
